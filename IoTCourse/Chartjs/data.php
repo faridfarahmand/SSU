15cafdca389bfd314ecb5a7a68c95f98dd63e8b3
faridfarahmand <faridfarahmand@gmail.com>
2022-10-19 15:10:09 -0700

    Update data.php

--- IoTCourse/Chartjs/data.php
+++ IoTCourse/Chartjs/data.php
@@ -4,9 +4,9 @@ header('Content-Type: application/json');
 
 //database
 define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'id19682308_db_faridfarahmand');
-define('DB_PASSWORD', 'SsuIotCourse!234');
-define('DB_NAME', 'id19682308_sensor');
+define('DB_USERNAME', 'id108_db_faridfarahmand');
+define('DB_PASSWORD', 'Ssu234');
+define('DB_NAME', 'id1908_sensor');
 
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -26,9 +26,6 @@ $data = array();
 foreach ($result as $row) {
   $data[] = $row;
 }
-
-//free memory associated with result
-$result->close();
 
 //close connection
 $mysqli->close();
