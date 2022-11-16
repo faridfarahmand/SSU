a576a6afd477b6e53a6d2912e44745c6d0dffa94
faridfarahmand <faridfarahmand@gmail.com>
2022-11-15 19:12:48 -0800

    Create SampleSQLSensorData.sql

---
+++ IoTCourse/dataBase/SampleSQLSensorData.sql
@@ -0,0 +1,8 @@
+NOTES: 
+--| Theses are sample database command to generate a quick table for your sensor data. 
+--| We are assuming teh table has teh following columns: 
+--| Node; ArrivalTime; Temp 
+--| node_1; ArrivalTime; Temp
+
+
+--| Link to this page: https://github.com/faridfarahmand/SSU/blob/main/IoTCourse/dataBase/SampleSQLSensorData.sql.sql 
